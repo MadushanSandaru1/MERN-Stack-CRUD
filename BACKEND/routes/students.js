3f429e25e273657caf21187ff6376f7cbdd7cef5
Madushan Sandaruwan <madushansandaru1@gmail.com>
2021-05-29 02:04:31 +0530

    Initial commit

---
+++ BACKEND/routes/students.js
@@ -0,0 +1,78 @@
+const router = require('express').Router();
+
+let Student = require('../models/Student');
+
+router.route('/add').post((req, res) => {
+
+    const name = req.body.name;
+    const age = Number(req.body.age);
+    const gender = req.body.gender;
+
+    const newStudent = new Student({
+        name,
+        age,
+        gender
+    });
+
+    newStudent.save().then(() => {
+        res.json('Student added');
+    }).catch((err) => {
+        console.log(err.message);
+    });
+
+});
+
+router.route('/').get((req, res) => {
+
+    Student.find().then((students) => {
+        res.json(students);
+    }).catch((err) => {
+        console.log(err);
+    });
+
+});
+
+router.route('/update/:id').put(async (req, res) => {
+
+    let userId = req.params.id;
+    const {name, age, gender} = req.body;
+
+    const updateStudent = {
+        name,
+        age,
+        gender
+    };
+
+    const update = await Student.findByIdAndUpdate(userId, updateStudent).then(() => {
+        res.status(200).send({status:'user updated'});
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).send({status:'Error with updating data'});
+    });
+
+});
+
+router.route('/delete/:id').delete(async (req, res) => {
+    
+    let userId = req.params.id;
+    await Student.findByIdAndDelete(userId).then(() => {
+        res.status(200).send({status : 'User deleted'});
+    }).catch((err) => {
+        console.log(err.message);
+        res.status(500).send({status : 'Error with deleting data'});
+    });
+
+});
+
+router.route('/:id').get(async (req, res) => {
+    let userId = req.params.id;
+
+    const user = await Student.findById(userId).then(() => {
+        res.status(200).send({status : 'User fetched', user : user});
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).send({status : 'Error with fetching data'});
+    });
+});
+
+module.exports = router;
---
+++ BACKEND/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('mongodb connection successfull');
+});
+
+const studentRouter = require('./routes/students.js');
+
+app.use('/student', studentRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port number: ${PORT}`);
+});
